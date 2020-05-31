55fa4bfe1b96b0e325caa069876c38145f3c20e1
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-31 13:44:01 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,29 +5,10 @@
 require 'pry'
 =begin
 
+
 =end 
 
-class EmailAddressParser
-  attr_accessor :list  
-  
-  def initialize(list)
-    @list = list
-  end 
-  
-  def parse
-    parsed = list.split(' ')
-    parsed2 = []
-    #binding.pry
-    parsed.each do |email| 
-      if email.end_with?(',')
-        email = email[0...-1]
-        parsed2 << email
-      else 
-        parsed2 << email 
-      end 
-  end 
-  parsed2.flatten.uniq
-end 
+
 
 
 end 
