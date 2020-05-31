3582a6ef752e3f24038de75a4ab0efce7821280a
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-31 13:47:58 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,30 +3,6 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 require 'pry'
-=begin
-class EmailAddressParser
-  attr_accessor :list  
-  
-  def initialize(list)
-    @list = list
-  end 
-  
-  def parse
-    parsed = list.split(' ')
-    parsed2 = []
-    #binding.pry
-    parsed.each do |email| 
-      if email.end_with?(',')
-        email = email[0...-1]
-        parsed2 << email
-      else 
-        parsed2 << email 
-      end 
-  end 
-  parsed2.flatten.uniq
-end 
-
-=end 
 
 =begin 
 class EmailAddressParser
@@ -51,6 +27,7 @@ class EmailAddressParser
 end 
 =end 
 
+=begin
 class EmailAddressParser
   attr_accessor :list  
   
@@ -69,10 +46,6 @@ class EmailAddressParser
   end 
   .flatten.uniq
 end 
-
-
-
-
-
+=end 
 
 end 
